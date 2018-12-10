ed4e7592936ff72eff4791ea69c96d8fd7a2d6fd
Jakub Onderka <jakub.onderka@gmail.com>
2018-12-10 10:28:10 +0100

    Added sha256 to microsoft-azure-storage-explorer

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-azure-storage-explorer' do
   version :latest
-  sha256 :no_check
+  sha256 'ce8d2cd5c01b471039f1aea75605fefe7e424dcd653a0c5426eee478c4e92de6'
 
   url 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer_signed.zip'
   name 'Microsoft Azure Storage Explorer'
